50b94dcbd7c3d6ca396384cf0bd78a592c39daf1
Sam Caldwell <samcaldwell@utexas.edu>
2013-05-02 16:59:57 -0500

    updated register file for checkout

--- REG.v
+++ REG.v
@@ -1,15 +1,18 @@
-module REG(CLK, RegW, DR, SR1, SR2, Reg_In, ReadReg1, ReadReg2, Reg_One_LSB);
+module REG(CLK, RegW, DR, SR1, SR2, Reg_In, ReadReg1, ReadReg2, 
+           iSW0, iSW1, iSW2, oR2, oR3);
     input CLK;
     input RegW;
     input [4:0] DR;
     input [4:0] SR1;
     input [4:0] SR2;
     input [31:0] Reg_In;
-    output [7:0] Reg_One_LSB;
+    input iSW0, iSW1, iSW2;
+    output [31:0] oR2, oR3;
     output reg [31:0] ReadReg1;
     output reg [31:0] ReadReg2;
     reg [31:0] REG [0:31];
     integer i;
+
     initial
     begin
         ReadReg1 = 0;
@@ -19,12 +22,18 @@ module REG(CLK, RegW, DR, SR1, SR2, Reg_In, ReadReg1, ReadReg2, Reg_One_LSB);
           end                     
     end
     
-    assign Reg_One_LSB = REG[1][7:0];
-    
+    assign oR2 = REG[2];
+    assign oR3 = REG[3];
+
     always @(posedge CLK)
     begin
+        REG[1][2:0] = {iSW2, iSW1, iSW0};
         if(RegW == 1'b1)
-            REG[DR] <= Reg_In[31:0];
+            if(DR != 1) begin
+                REG[DR] <= Reg_In[31:0];
+            end else begin
+                REG[DR][31:3] = Reg_In[31:3];
+            end
         ReadReg1 <= REG[SR1];
         ReadReg2 <= REG[SR2];
     end
